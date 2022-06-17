5ac76803b678565b74e821cb2bb71f5ec1f8e04b
Carlos Eduardo <carlos_e@id.uff.br>
2022-06-17 11:34:56 -0400

    17.06-11:35

--- 93 - Bib Queue (Fila)/FilaSeq2.cpp
+++ 93 - Bib Queue (Fila)/FilaSeq2.cpp
@@ -3,59 +3,72 @@
 
 using namespace std;
 
-constexpr int MAXN = 100; // capacidade máxima da fila
+constexpr int MAXN = 100000; // capacidade máxima da fila
 
-class FilaSeq1{
+class FilaSeq2
+{
+private:
 
-char elementos [MAXN];
-int N;
-void cria ();
-void libera ();
-char frente ();
-void enfileira (char dado);
-char desenfileira ();
+public:
+
+    char elementos[MAXN];
+    int N;
+    int inicio;
+    int fim;
+    void cria(){...};
+    void libera(){...};
+    char frente(){...};
+    void enfileira(char dado){...};
+    char desenfileira(){...};
 };
 
-void cria (){
-this->N = 0;    
+void cria()
+{
+    this->N = 0;
+    this->inicio = 0;
+    this->fim = 0;
 }
-char frente (){
-return this->elementos[0];
+char frente()
+{
+    return this->elementos[inicio];
 }
-void enfileira(char dado){
-this->elementos[N] = dado;
-this->N++;
+void enfileira(char dado)
+{
+    this->elementos[fim] = dado;
+    this->fim++;
+    this->N++;
 }
-char desenfileira(){
-char r = this->elementos[0];
-for (int i=0; i<N-1; i++) 
-this->elementos[i] = this->elementos[i+1]; 
-this->N--; 
-return r;
+char desenfileira()
+{
+    char r = this->elementos[inicio];
+    this->inicio++;
+    this->N--;
+    return r;
 }
-
-
-int main(){
-
-FilaSeq1 *p=new FilaSeq1();
-
-p->cria(); 
-p->enfileira('A'); 
-p->enfileira('B'); 
-p->enfileira('C');
-
-std::cout <<p->frente();
-std::cout <<p->desenfileira();
-
-p->enfileira('D');
-
-while (p->N > 0){
-cout <<p->desenfileira();
+void libera(){ // nenhum recurso dinâmico para desalocar 
 }
 
+int main()
+{
 
-p->libera();
+    FilaSeq2 *p = new FilaSeq2();
 
-return 0;
+    p->cria();
+    p->enfileira('A');
+    p->enfileira('B');
+    p->enfileira('C');
+
+    std::cout << p->frente();
+    std::cout << p->desenfileira();
+
+    // p->enfileira('D');
+
+    while (p->N > 0)
+    {
+        cout << p->desenfileira();
+    }
+
+    // p->libera();
+
+    return 0;
 }
-
